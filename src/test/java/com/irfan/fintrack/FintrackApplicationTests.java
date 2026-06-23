cedf6dde94b68cfb697178f688825183611f4b30
mirfankmrjk <mirfankm915@gmail.com>
2026-06-23 17:17:38 +0530

    Initial commit

---
+++ src/test/java/com/irfan/fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irfan.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
